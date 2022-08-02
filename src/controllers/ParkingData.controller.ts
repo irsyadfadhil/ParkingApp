1eddfdfd4814e21ad85c93a6655b2d674a9f7728
irsyadfadhil <irsyadfadhil@student.telkomuniversity.ac.id>
2022-08-02 20:11:41 +0700

    CRUD Tahap 3 & Test Jest JS

--- src/controllers/ParkingData.controller.ts
+++ src/controllers/ParkingData.controller.ts
@@ -11,8 +11,14 @@ export async function getParkingDatas(req: Request, res: Response): Promise<Resp
 
 export async function createParkingData(req: Request, res: Response) {
     const newPost: ParkingData = req.body;
+    const newlevel: ParkingData = req.body.level;
     const conn = await connect();
-    await conn.query('INSERT INTO parking_data SET ?', [newPost]);
+
+    for (let i = 1; i <= req.body.slot; i++){
+        const angka = i++;
+        await conn.query('INSERT INTO parking_data SET ?', [newPost]);
+    }
+
     return res.json({
         message: 'Parking Data Created'
     });
@@ -49,18 +55,41 @@ export async function InParkingData(req: Request, res: Response) {
     const id = req.params.id;
     const InParkingData: ParkingData = req.body;
     const conn = await connect();
+    const tempat_parkir = await conn.query('SELECT * FROM parking_data WHERE id = ?', [id]);
     await conn.query('UPDATE parking_data SET ? WHERE id = ?', [InParkingData, id]);
-    return res.json({
-        message: 'In Parking Data'
-    });
+    return res.json(tempat_parkir[0]);
+
+    // return res.json({
+    //     message: ' "level": "B1", "slot": "10" '
+    // });
 }
 
 export async function OutParkingData(req: Request, res: Response) {
     const id = req.params.id;
     const OutParkingData: ParkingData = req.body;
     const conn = await connect();
+    const tempat_parkir = await conn.query('SELECT * FROM parking_data WHERE id = ?', [id]);
     await conn.query('UPDATE parking_data SET ? WHERE id = ?', [OutParkingData, id]);
-    return res.json({
-        message: 'Out Parking Data'
-    });
+    return res.json(tempat_parkir[0]);
+
+    // return res.json({
+    //     message: 'Out Parking Data'
+    // });
 }
+
+// export async function updatearticle(req: Request) {
+//     Article.findOneAndUpdate(
+//         { "Username": req.body.username, "Email": req.body.email, "Info": "Deactivate" },
+//         { "$set": { "Info": "Active" } },
+//         { "new": true },
+//         function (err, doc) {
+//             if (err) { // err: any errors that occurred
+//                 console.log(err);
+//             } else { // doc: the document before updates are applied if `new: false`
+//                 console.log(doc); // , the document returned after updates if `new  true`
+//                 console.log(doc.Info);
+//             }
+
+//         }
+//     );
+// };
